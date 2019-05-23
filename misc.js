1d9addc7a5349af9c5c2675c35863902e968b51b
Kevin Yang <37386353+kv7870@users.noreply.github.com>
2019-05-22 22:42:33 -0400

    Add files via upload

--- misc.js
+++ misc.js
@@ -1,26 +1,34 @@
-//misc.js
+//p5.js function that is called when left-click button is pressed
 function mousePressed() {
   var hide = true;
+
+  //if in 'select' mode
   if(selectPressed) {
    for(var i=0; i<planet.length; i++) {
+     //check if cursor is on a planet at time of mouse click
      if( (mouseX-planet[i].x/SCALE)*(mouseX-planet[i].x/SCALE) + (mouseY-planet[i].y/SCALE)*(mouseY-planet[i].y/SCALE)
      <= planet[i].radius*planet[i].radius) {
        selectedPlanet = planet[i].id;
+       //if a planet is selected, show options menu for that planet
        showOptions();
-       console.log(planet[selectedPlanet].radius);
        hide = false;
-//break;
      }
    }
+
+   //hide options menu if user clicks on the background
    if(hide && mouseX < bx)
       hideOptions();
  }
+
+ //if in 'create' mode
  else if(addPressed) {
+   //prevent planet from spawning behind options menu
     if((drawBox && mouseX<bx) || !drawBox) {
       selectedPlanet = id;
       var r = Math.random()*(256-100)+100;
       var g = Math.random()*(256-100)+100;
       var b = Math.random()*(256-100)+100;
+      //create planet at coordinates of cursor at time of mouse click
       var tempPlanet = new Body("Planet " + (id+1), 5.972e20, 10, mouseX*SCALE, mouseY*SCALE, -1000, -3030.0,
       r,g,b);
       planet.push(tempPlanet);
@@ -29,9 +37,11 @@ function mousePressed() {
   }
 }
 
+//function called when "New planet" button is pressed
 function toggleAdd() {
   addPressed = true;
   selectPressed = false;
+  //change colour of buttons
   bAdd.style('background-color', '#00ff00');
   bSelect.style('background-color', '#ffffff');
 }
@@ -39,10 +49,12 @@ function toggleAdd() {
 function toggleSelect() {
   selectPressed = true;
   addPressed = false;
+  //change colour of buttons
   bSelect.style('background-color', '#00ff00');
   bAdd.style('background-color', '#ffffff');
 }
 
+//show options menu (sliders and buttons)
 function showOptions() {
   console.log(selectedPlanet);
   drawBox = true;
@@ -73,6 +85,7 @@ function showOptions() {
 }
 
 
+//hide options menu
 function hideOptions() {
     //button.html("Show");
     drawBox = false;
@@ -87,22 +100,26 @@ function hideOptions() {
     redSlider.hide();
     greenSlider.hide();
     blueSlider.hide();
-
 }
 
 
-//axis aligned bounding box
+//aabb = axis aligned bounding box
+//Bounding boxes refers to the square in which the circular planet is inscribed.
+//To reduce computation, check if the boxes are overlapping before checking if
+//circles are touching
 function aabb() {
 
   for(var i=0; i<planet.length; i++) {
-    //bounding boxes are overlapping
+
     for(var j=0; j<planet.length; j++) {
       if(planet[j].id!=planet[i].id) {
+        //determine if bounding boxes are overlapping
         if (planet[i].x + planet[i].radius*SCALE + planet[j].radius*SCALE > planet[j].x
           && planet[i].x < planet[j].x + planet[i].radius*SCALE + planet[j].radius*SCALE
           && planet[i].y + planet[i].radius*SCALE + planet[j].radius*SCALE > planet[j].y
           && planet[i].y < planet[j].y + planet[i].radius*SCALE + planet[j].radius*SCALE) {
 
+            //distance between centers of the two planets
             var dist = Math.sqrt( (planet[i].x - planet[j].x) * (planet[i].x - planet[j].x)
             + (planet[i].y - planet[j].y) * (planet[i].y - planet[j].y) );
 
@@ -110,18 +127,22 @@ function aabb() {
             if (dist/SCALE < planet[i].radius + planet[j].radius) {
               id--;
 
+              //the planet with the smaller mass gets destroyed
               if(planet[i].mass > planet[j].mass) {
+                //close options menu if menu is open for the planet that got destroyed
                 if(selectedPlanet == j) {
                   selectedPlanet = -1;
                   hideOptions();
                 }
+                //in the array, if the planet for which the options menu is displayed comes after the
+                //planet that has been destroyed, decrement the selected planet's ID
                 else if(selectedPlanet > j)
                   selectedPlanet--;
 
                 for(var n = j+1; n<planet.length; n++) {
                   (planet[n].id)--;
                 }
-
+                //pop the planet that got destroyed
                 planet.splice(j, 1);
               }
 
@@ -150,9 +171,9 @@ function aabb() {
 }
 
 
-function updateMass() {
+/*function updateMass() {
   planet[selectedPlanet].mass = mSlider.value();
-}
+}*/
 
 function updateVx() {
   planet[selectedPlanet].vx = vxSlider.value();
--- sketch.js
+++ sketch.js
@@ -1,45 +1,66 @@
+//universal gravitational constant
 var G = 6.67408e-11;
 
+//draw options box
 var drawBox = false;
+//coordinates of options box
 var bx = 1600, by = 0, bw = 1600, bh = 937;
 
+//array of planets
  var planet = [];
- var n = 0;
+
+ //time step (delta time) used in RK4
  var dt = 86400.0;
 
+//mass of Earth in kg
  var earthM = 5.972e24;
+//radius of Earth in pixels
  var earthR = 10; //earth radius = 5 px
+//distance from Earth to Sun in metres; this will serve as unit of distance
  var distES = 1.49e11;
 
+//tracks whether "add planet" button is pressed
 var addPressed = false;
+//tracks whether "select" button is pressed
 var selectPressed = false;
+
+//id of planet that user clicks on
 var selectedPlanet = -1;
+
+//id assigned to each newly spawned planet
 var id = 0;
 
+//# of dt passed since beginning
 var timeElapsed = 0;
- //149600000000;
- //distES = 149600000000; want dist earth to neptune, which is 18*distES,
- //so 1920/18 = 106.6
- //therefore distES represented by 100 pixels
 
- var SCALE = distES/100;  //scale distance
+//want to represent distance from Earth to Neptune on screen;
+//dist Earth-Neptune is ~18 * dist Earth-Sun; canvas is 1900 pixels wide, and 1900/18 ~= 100
+//therefore let 100 pixels represent dist Earth-Sun
+ var SCALE = distES/100;
+
+//resolution
+var SCREEN_W = 1900;
+var SCREEN_H = 937;
 
  planet[0] = new Body("Earth", earthM, earthR, 900 *SCALE, 440 *SCALE, 30300, 0, 0, 255, 0);
  planet[1] =  new Body("Sun", earthM*333000, earthR*5, 960 *SCALE, 540 *SCALE, 0, 0, 255, 255, 0);
 
+//planet "class"; stores data about the planet
  function Body(n,m,r,posX,posY,velocityX,velocityY, red, green, blue) {
-  this.name = n;
-  this.mass = m;
-  this.radius = r;
-  this.x = posX;
-  this.y = posY;
-  this.x0 = posX;
-  this.y0 = posY;
-  this.vx = velocityX;
-  this.vy = velocityY;
-  this.ax = 0;
-  this.ay  = 0;
-  this.id = id++;
+  this.name = n;          //name of planet
+  this.mass = m;          //mass of planet in kg
+  this.radius = r;        //radius of planet in metres
+  this.x = posX;          //x-coordinate of planet (in pixels)
+  this.y = posY;          //y-coordinate of planet  (in pixels)
+  this.x0 = posX;         //initial x-coord of planet
+  this.y0 = posY;         //initial y-coord of planet
+  this.vx = velocityX;    //horizontal velocity of planet (m/s)
+  this.vy = velocityY;    //vertical velocity of planet (m/s)
+  this.ax = 0;            //horizontal acceleration of planet (m/s^2)
+  this.ay  = 0;           //vertical acceleration of planet (m/s^2)
+  this.id = id++;         //ID of planet (0,1,2,3...)
+
+  //RGB values of planet
   this.red = red;
   this.green = green;
   this.blue = blue;
@@ -47,18 +68,23 @@ var timeElapsed = 0;
   //member functions
   this.updateTrace = updateTrace;
 
+  //calculate planet's acceleration due to gravity
   this.calcAcceleration = calcAcceleration;
 
+  //integrate planet's acceleration using RK4 to obtain next (x, y)
   this.rk4 = rk4;
 }
 
 
+//setup() is a p5.js function that is called on startup
 function setup() {
-  createCanvas(1900,937);
+  createCanvas(SCREEN_W,SCREEN_H);
   frameRate(60);
 
   fill(0,0,0);
   space = loadImage('assets/background.jpg');
+
+  //create and initialize the "X" button on the corner of the options box
   close = createButton("X");
   close.position(bx,by);
   close.size(50,50);
@@ -66,26 +92,32 @@ function setup() {
   close.style('background-color', "#ff0000");
   close.style('color', "#ffffff");
 
+  //slider that adjusts planet's mass
   mSlider = createSlider(0, earthM*3330000, 66.6e3);
   mSlider.style('width', '275px');
   mSlider.position(bx+15, by+170);
 
+  //slider that adjusts radius
   rSlider = createSlider(1,500,250);
   rSlider.position(bx+15, by+270);
   rSlider.style('width','275px');
 
+  //slider that adjusts horizontal velocity
   vxSlider = createSlider(-50000, 50000, 0);
   vxSlider.position(bx+15, by+370);
   vxSlider.style('width','275px');
 
+  //slider that adjusts vertical velocity
   vySlider = createSlider(-50000, 50000, 0);
   vySlider.position(bx+15, by+470);
   vySlider.style('width','275px');
 
+  //slider that adjusts dt (time step, i.e. simulation speed)
   speedSlider = createSlider(0, 864000, 86400);
   speedSlider.position(bx+15, by+820);
   speedSlider.style('width', '275px');
 
+  //sliders that adjust RGB value of planet
   redSlider = createSlider(0, 255, 128);
   redSlider.position(bx+15, by+570);
   redSlider.style('width', '275px');
@@ -98,38 +130,52 @@ function setup() {
   blueSlider.position(bx+15, by+710);
   blueSlider.style('width', '275px');
 
+  //input box that displays and lets user enter planet's name
   input = createInput("");
   input.position(bx+85,by+85);
   input.style('width', '130px');
 
+  //button that assigns user's input to planet's name
   submit = createButton("Submit");
   submit.position(bx+230, by+85);
 
+  //"Select" button which, when pressed,
+  //lets user click on existing planets to adjust its properties
   bSelect = createButton("Select");
   bSelect.position(930, 850);
   bSelect.style('width', '125px');
   bSelect.style('height', '50px');
   bSelect.style('font-size', '18px');
 
+  //"New planet" button which, when pressed,
+  //lets user spawn a new planet at location of mouse click
   bAdd = createButton("New planet");
   bAdd.position(700, 850);
   bAdd.style('width', '125px');
   bAdd.style('height', '50px');
   bAdd.style('font-size', '18px');
 
+  //hide buttons, sliders, and options box on startup
   hideOptions();
 }
 
+//p5.js function that is automatically called 60 times per second
 function draw() {
-  clear();
-  timeElapsed+=dt;
-  background(0);
-  image(space, 0, 0);
+  clear();  //clear canvas to create animation
 
+  timeElapsed += dt;
+  background(0);
+  image(space, 0, 0); //draw space background
+
+  //if "X" button is pressed, hide options box
   close.mousePressed(hideOptions);
+  //if "select" button is pressed, call toggleSelect(), which enters 'selection mode'
   bSelect.mousePressed(toggleSelect)
+
+  //if "New planet" button is pressed, call toggleAdd(), which enters 'create' mode
   bAdd.mousePressed(toggleAdd);
 
+  //check collision between planets using axis-aligned bounding boxes
   aabb();
 
   for(var i=0; i<planet.length; i++) {
@@ -138,81 +184,119 @@ function draw() {
 
       circle(planet[i].x/SCALE, planet[i].y/SCALE, planet[i].radius*2);
       planet[i].rk4();
-      n++;
     }
 
     fill(255,255,255);
     textSize(24);
-    //text("HI", 100, 100);
     text("Time Elapsed: "+nf(timeElapsed/86400, 0, 0)+" days", 860, 75);
+
+    //draw options box
     if(drawBox) {
       fill(119,136,153);    //112,128,144
       rect(bx, by, bw, bh);
       textSize(18);
       fill(0);
-      text("Name", bx+15, by+100);
-      text("Mass", bx+135,by+160);
-      text("Radius", bx+130, by+260);
-      text("Horizontal Veloctiy", bx+90, by+360);
-      text("Vertical Veloctiy", bx+95, by+460);
-      text("Red", bx+140, by+560);
-      text("Green", bx+135, by+630);
-      text("Blue", bx+139, by+710);
-      text("Simulation speed", bx+96, by+810);
-      //text("Orbit eccentricity: ", bx+15, by+660);
-      mSlider.input(updateMass);
+      text("Name:", bx+15, by+100);
+      text("Mass:", bx+25,by+160);
+      text((planet[selectedPlanet].mass).toExponential(3) + " kg", bx+110, by+160);
+
+      text("Radius:", bx+25, by+260);
+      text(nf((planet[selectedPlanet].radius*SCALE)/1000, 0, 0) + " km", bx+110, by+260);
+
+      text("Velocity (x):", bx+25, by+360);
+      text(nf(planet[selectedPlanet].vx, 0, 3) + " m/s", bx+140, by+360);
+
+      text("Velocity (y):", bx+25, by+460);
+      text(nf(planet[selectedPlanet].vy, 0, 3) + " m/s", bx+140, by+460);
+
+      text("Red:", bx+25, by+560);
+      text(nf(planet[selectedPlanet].red, 0, 0), bx+92, by+560);
+
+      text("Green:", bx+25, by+630);
+      text(nf(planet[selectedPlanet].green, 0, 0), bx+95, by+630);
+
+      text("Blue:", bx+25, by+710);
+      text(nf(planet[selectedPlanet].blue, 0, 0), bx+95, by+710);
+
+      text("Simulation speed:", bx+25, by+810);
+      text(nf(dt/86400, 0, 3) + " day(s)", bx+180, by+810);
+
+      //since vx and vy are constantly changing, only make vx & vy match their
+      //respective slider position when the user adjusts the slider
       vxSlider.input(updateVx);
       vySlider.input(updateVy);
-      planet[selectedPlanet].radius = rSlider.value();
+      //mSlider.input(updateMass);
 
+      //since the following values are not constantly changing, they can be set
+      //to a constant corresponding to the slider's position
+      planet[selectedPlanet].mass = mSlider.value();
+      planet[selectedPlanet].radius = rSlider.value();
       dt = speedSlider.value();
       planet[selectedPlanet].red = redSlider.value();
       planet[selectedPlanet].green = greenSlider.value();
       planet[selectedPlanet].blue = blueSlider.value();
 
       submit.mousePressed(editName);
-
     }
 }
 
-
+//set planet's initial coordinates to its current coordinates in preparation
+//for next round of RK4
 function updateTrace() {
     this.x0  = this.x;
     this.y0  = this.y;
 }
 
-function calcAcceleration(x, y)
-{
+//member function that calculates the net acceleration of the planet in question (this)
+function calcAcceleration(x, y) {
   this.ax = 0;
   this.ay = 0;
 
+  //calculate each planet's gravitational pull on the planet in question,
+  //and later add them up to obtain that planet's net acceleration
   for(var j=0; j<planet.length; j++) {
     if(this.id!=planet[j].id) {
-      var Mj = planet[j].mass;
+      //'j' subscript refers to other planets
+      var mj = planet[j].mass;
       var xj = planet[j].x0;
       var yj = planet[j].y0;
+
+      //distance between centers of planet[j] and planet[i]
       var dx = xj-x;
       var dy = yj-y;
-      //var g = G * Mj;
-      //var d3Inv = 1 / (Math.pow(  ((xj - x)*SCALE) * ((xj - x)*SCALE) + ((yj - y)*SCALE) * ((yj - y)*SCALE), 1.5) );
 
+      //The following section uses Newton's 2nd law & law of universal gravitation to calculate acceleration;
+      //Fnet = ma, and Fnet = sum Fg;
+      //ma = (G * m * mj)/(d^2);
+      //Cancel m from both sides to get a = (G * mj)/(d^2);
+      //Split into components and written in vector form:
+      //ax = (G * mg *(x-xj)) / (d^3)
+      //ay = (G * mg *(j-jj)) / (d^3)
+      //See lined paper for more details on derivation
+
+      //calculate the distance between the centers of the two planets using the distance formula;
+      //since d is in metres, it must be converted to pixels by dividing by SCALE
+      //so that it matches the units of radius (in pixels)
       var d = Math.pow(dx*dx + dy*dy, 0.5);
 
+      //check that the two planets have not collided
       if(d/SCALE > this.radius + planet[j].radius) {
-        this.ax += (G * Mj * dx/Math.pow(d, 3));
-        this.ay += (G * Mj * dy/Math.pow(d, 3));
+        //sum up the acceleration of the planet in question
+        this.ax += (G * mj * dx/Math.pow(d, 3));
+        this.ay += (G * mj * dy/Math.pow(d, 3));
       }
     }
-  //this.ax = (g * SCALE*(xj - x) * d3Inv);
-  //this.ay = (g * SCALE*(yj - y) * d3Inv);
-    }
+  }
 }
 
+//integrate acceleration with Runge-Kutta 4th order
 function rk4() {
      var x  = this.x;
      var y  = this.y;
      var vx = this.vx;
      var vy = this.vy;
+
+     //there are 4 k's for x,y, and 4 k's for vx, vy
      var kx = [];
      var ky = [];
      var kvx = [];
@@ -225,55 +309,64 @@ function rk4() {
       kvy[i]=0;
      }
 
-     var tx = [], ty = [], tvx = [], tvy = [];
+     //Position is a function of time,
+     //velocity is a function of position (derivative of position), and
+     //acceleration is a function of velocity (derivative of velocity).
+     //Thus, integrating acceleration gives the rate at which velocity is changing
+     //with respect to time, and integrating velocity gives the
+     //rate at which position is changing w.r.t to time, which gives an estimate of
+     //the planet's next (x,y) coordinates
 
+    //Integration is accomplished numerically with RK4, which consists of 4 stages for greater stability.
+    //The 4 k values of each stage are calculated from the k values of the previous stage.
+    //Adding up the k values (which can be thought of as change in position)
+    //in a certain way and multiplying by dt gives the new (x,y) position.
 
-       //stage 1
-    tx[0] = vx;
-    ty[0] = vy;
+    //The following is based on the standard RK4 formula; see documentation for more details
+
+    //stage 1
+    kx[0] = vx;
+    ky[0] = vy;
 
     this.calcAcceleration(x, y);
 
-     tvx[0] = this.ax;
-     tvy[0] = this.ay;
+    kvx[0] = this.ax;
+    kvy[0] = this.ay;
 
-       // stage2
-     tx[1] = vx + 0.5 * dt * tvx[0];
-     ty[1] = vy + 0.5 * dt * tvy[0];
+    //stage 2
+    kx[1] = vx + 0.5 * dt * kvx[0];
+    ky[1] = vy + 0.5 * dt * kvy[0];
 
-     this.calcAcceleration(x+0.5*dt* tx[0], y+ 0.5 * dt * ty[0]);
+    this.calcAcceleration(x+0.5*dt* kx[0], y+ 0.5 * dt * ky[0]);
 
-     tvx[1] = this.ax;
-     tvy[1] = this.ay;
+    kvx[1] = this.ax;
+    kvy[1] = this.ay;
 
-       // stage 3
-     tx[2] = vx + 0.5 * dt * tvx[1];
-     ty[2] = vy + 0.5 * dt * tvy[1];
+    //stage 3
+    kx[2] = vx + 0.5 * dt * kvx[1];
+    ky[2] = vy + 0.5 * dt * kvy[1];
 
-     this.calcAcceleration(x + 0.5 * dt * tx[1], y + 0.5 * dt * ty[1]);
+    this.calcAcceleration(x + 0.5 * dt * kx[1], y + 0.5 * dt * ky[1]);
 
-     tvx[2] = this.ax;
-     tvy[2] = this.ay;
+    kvx[2] = this.ax;
+    kvy[2] = this.ay;
 
-       // stage 4
-     tx[3] = vx + dt * tvx[2];
-     ty[3] = vy + dt * tvy[2];
+    //stage 4
+    kx[3] = vx + dt * kvx[2];
+    ky[3] = vy + dt * kvy[2];
 
-     this.calcAcceleration(x + dt * tx[2], y + dt * ty[2]);
+    this.calcAcceleration(x + dt * kx[2], y + dt * ky[2]);
 
-     tvx[3] = this.ax;
-     tvy[3] = this.ay;
+    kvx[3] = this.ax;
+    kvy[3] = this.ay;
 
-     for (var k = 0; k < 4; k++) {
-       kx[k]  += tx[k];
-       ky[k]  += ty[k];
-       kvx[k] += tvx[k];
-       kvy[k] += tvy[k];
-    }
+    //position's rate of change * delta time = change in position;
+    //add the change in position to the current (x,y) coordinates
+    //to obtain the planet's new position
+    this.x += ((kx[0] + 2 * (kx[1] + kx[2]) + kx[3]) * dt / 6);
+    this.y += ((ky[0] + 2 * (ky[1] + ky[2]) + ky[3]) * dt / 6);
 
-  this.x += ((kx[0] + 2 * (kx[1] + kx[2]) + kx[3]) * dt / 6);
-  this.y += ((ky[0] + 2 * (ky[1] + ky[2]) + ky[3]) * dt / 6);
-
-  this.vx += ((kvx[0] + 2 * (kvx[1] + kvx[2]) + kvx[3]) * dt / 6);
-  this.vy += ((kvy[0] + 2 * (kvy[1] + kvy[2]) + kvy[3]) * dt / 6);
+    //do the same to obtain the planet's new velocity
+    this.vx += ((kvx[0] + 2 * (kvx[1] + kvx[2]) + kvx[3]) * dt / 6);
+    this.vy += ((kvy[0] + 2 * (kvy[1] + kvy[2]) + kvy[3]) * dt / 6);
 }
