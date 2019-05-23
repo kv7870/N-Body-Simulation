0a193e8f351008ca9ebbe3b72f8524dff709c842
Kevin Yang <37386353+kv7870@users.noreply.github.com>
2019-05-22 23:10:52 -0400

    Update misc.js

--- misc.js
+++ misc.js
@@ -25,9 +25,9 @@ function mousePressed() {
    //prevent planet from spawning behind options menu
     if((drawBox && mouseX<bx) || !drawBox) {
       selectedPlanet = id;
-      var r = Math.random()*(256-100)+100;
-      var g = Math.random()*(256-100)+100;
-      var b = Math.random()*(256-100)+100;
+      var r = Math.random()*255;
+      var g = Math.random()*255
+      var b = Math.random()*255;
       //create planet at coordinates of cursor at time of mouse click
       var tempPlanet = new Body("Planet " + (id+1), 5.972e20, 10, mouseX*SCALE, mouseY*SCALE, -1000, -3030.0,
       r,g,b);
